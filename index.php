39ae754fee75b6276db12815457f850aa2f8832f
Martin Kokeš <info@martinkokes.cz>
2020-12-09 13:45:05 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+include_once 'babcafunction.php';
+include_once 'klimiFail.php';
+include_once 'knefiFail.php';
+include_once 'pilarfail.php';
+include_once 'pojerFail.php';
+?>
+
+<img style="display:block; max-width: 100%; height: 100%;" src="data:image/jpeg;base64, <?= $img ?>" alt="hidden image" srcset="">
